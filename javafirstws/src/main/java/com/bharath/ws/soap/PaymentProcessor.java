a0c171e566916ee4406c878b862047c470e590d8
Heloa Silva <hsilva25@its.jnj.com>
2020-04-13 21:32:22 -0300

    add javafirstws

---
+++ javafirstws/src/main/java/com/bharath/ws/soap/PaymentProcessor.java
@@ -0,0 +1,10 @@
+package com.bharath.ws.soap;
+
+import com.bharath.ws.soap.dto.PaymentProcessorRequest;
+import com.bharath.ws.soap.dto.PaymentProcessorResponse;
+
+
+public interface PaymentProcessor {
+	
+	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest);
+}
